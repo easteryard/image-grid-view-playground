8f1ef56e72c331b26a61fe82e42b0b1665b7feaf
Casper Østergaard <casper@acto.dk>
2021-03-20 04:20:29 +0100

    Removed redundant code in ImageModal

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -4,16 +4,11 @@ import { makeStyles } from '@material-ui/core/styles'
 import ImageSlides from 'react-imageslides'
 
 import { Close as CloseIcon } from '@material-ui/icons'
-import { IconButton, SvgIcon } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
 
-const useStyles = makeStyles(theme => ({
-    outerDiv: {
-        // position: 'relative'
-    },
+const useStyles = makeStyles(() => ({
     closeIcon: {
         color: 'white'
-        // top: 0,
-        // right: 0
     }
 }))
 
@@ -31,7 +26,7 @@ export default function ImageModal ({ images, initialImageIdx, onClose }: IProps
     })
 
     return (
-        <div className={classes.outerDiv} onKeyDown={e => onClose(e.key)}>
+        <div onKeyDown={e => onClose(e.key)}>
             <ImageSlides images={images} isOpen={initialImageIdx !== undefined} index={initialImageIdx && initialImageIdx + 1} showPageButton
                          onClose={() => onClose('Escape')} addon={() => (
                              <IconButton onClick={e => onClose(e.type)} onKeyDown={e => onClose(e.key)} tabIndex={0}>
